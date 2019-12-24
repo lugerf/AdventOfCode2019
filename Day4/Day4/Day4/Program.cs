6d6afcf6ae446648cb86e740ed816a4a381c9ee6
Florian Luger <F.Luger@hotmail.com>
2019-12-24 16:38:14 +0100

    24.12.2019: Day 4, Task 1

---
+++ Day4/Day4/Day4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4
+{
+    class Program
+    {
+        /// <summary>
+        /// Task:
+        /// For range: 372304 - 847060
+        /// Get the number of passwords which:
+        /// - two adjacent numbers are the same
+        /// - from left to right digits never decrease
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Task 1:
+            // Init
+            List<int> taskOneResult = new List<int>();
+
+            // Processing
+            for (int i = 372304; i < 847060; i++)
+            {
+                if (StaticHelper.AdjacentNumbersInCode(i.ToString()) && StaticHelper.AllDigitsDoIncrease(i.ToString()))
+                {
+                    taskOneResult.Add(i);
+                }
+            }
+
+            Console.WriteLine($"The number of matching codes are: {taskOneResult.Count}");
+        }
+
+       
+    }
+}
---
+++ Day4/Day4/Day4/StaticHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Day4
+{
+    static class StaticHelper
+    {
+        #region Helpers
+        /// <summary>
+        /// Checks if an input code has digits which are the same and are next to each other. Returns a boolean value.
+        /// </summary>
+        /// <param name="codeToCheck"></param>
+        /// <returns></returns>
+        public static bool AdjacentNumbersInCode(string codeToCheck)
+        {
+            bool result = false;
+            char lastDigit = new char();
+
+            for (int i = 0; i < codeToCheck.Length; i++)
+            {
+                if (codeToCheck[i] == lastDigit)
+                {
+                    result = true;
+                }
+                lastDigit = codeToCheck[i];
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks if an input code has only increasing digits from left to right. Returns a boolean value. 
+        /// </summary>
+        /// <param name="codeToCheck"></param>
+        /// <returns></returns>
+        public static bool AllDigitsDoIncrease(string codeToCheck)
+        {
+            bool result = true;
+            char lastDigit = new char();
+
+            for (int i = 0; i < codeToCheck.Length; i++)
+            {
+                if (codeToCheck[i] >= lastDigit)
+                {
+                    // result still true
+                }
+                else
+                {
+                    // criteria not met
+                    result = false;
+                }
+                lastDigit = codeToCheck[i];
+            }
+            return result;
+        }
+        #endregion
+    }
+}
